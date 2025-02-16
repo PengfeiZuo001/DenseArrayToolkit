81a6157ac4e3e506fe8144ca56532b57a2dce614
BIGSTARRR <12238028@zju.edu.cn>
2025-02-16 20:00:36 +0800

    add HKstacking

--- preprocessing/preprocessing.m
+++ preprocessing/preprocessing.m
@@ -134,7 +134,8 @@ for n = 1:length(DataStruct)
         
         % Save to DataStruct
         DataStruct(n).TravelInfo.pTime    = pTime;
-        DataStruct(n).TravelInfo.rayParam = ptime(1).rayParam;     % s/rad
+%         DataStruct(n).TravelInfo.rayParam = ptime(1).rayParam;     % s/rad
+        DataStruct(n).TravelInfo.rayParam = ptime(1).rayParam/6371;
     catch ME
         msgPT = sprintf('[Error] taupTime: %s', ME.message);
         DataStruct(n).ProcHistory{end+1} = msgPT;
