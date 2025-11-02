0c03653b8d7d2dcfeb143b8cd3cf44b637a0e5f2
PengfeiZuo001 <pengfei080@gmail.com>
2025-11-01 19:56:12 -0700

    Update copyright year
    Updated copyright year from 2024 to 2025.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,12 +1,10 @@
 import os
 import sys
 
-# 添加项目根目录到Python路径
 sys.path.insert(0, os.path.abspath('../..'))
 
-# 项目信息
 project = 'DenseArrayToolkit'
-copyright = '2024, DAT v1.0.0'
+copyright = '2025, DAT v1.0.0'
 author = 'Seismology Group'
 
 
@@ -40,8 +38,6 @@ myst_enable_extensions = [
 
 myst_url_schemes = ["http", "https", "mailto"]
 
-# 主题设置
 html_theme = 'sphinx_rtd_theme'
 
-# 静态文件路径
 html_static_path = ['_static']
